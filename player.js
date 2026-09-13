6bb310f93bf2da785e5d63e7abe2bf0a924d2d81
nidmegent <lgs.esportsgames@gmail.com>
2026-09-13 22:47:27 +0900

    Delete kanzaki and nade player data
    Removed kanzaki and nade player entries from player.js.

--- player.js
+++ player.js
@@ -55,33 +55,6 @@ results: [
 {date: "大会出場：",title: "桜珂杯",rank: "2位"}
 ]},
 
-kanzaki: {
-role: "CREATOR",
-name: "KanzakiAyaka",
-realName: "JOIND：2026.6",
-birthday: "BIRTHDAY：09/21",
-image: "player/kanzaki.png",
-x: "https://x.com/kknzkk1129",
-youtube: "https://www.youtube.com/@knzk_09",
-twitch: "https://www.twitch.tv/knzk_09",
-description: "ポジション思考？なゲーマー",
-results: [
-{date: "大会運営：",title: "STELLA CUP",rank: ""}
-]},
-
-nade: {
-role: "CREATOR",
-name: "Nade",
-realName: "JOIND：2026.8",
-birthday: "BIRTHDAY：07/21",
-image: "player/nade.png",
-x: "#",
-youtube: "#",
-twitch: "#",
-description: "Nidmegentイメージキャラクター",
-results: []
-}
-
 }; // playerstats end
 
 /*==================================================
