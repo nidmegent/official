ccdff9e7b23cedaa23404a18dff5d71c8255614f
nidmegent <lgs.esportsgames@gmail.com>
2026-09-14 15:35:46 +0900

    Update player.js

--- player.js
+++ player.js
@@ -25,6 +25,7 @@ results: [
 {date: "大会運営：",title: "StreamerValorantCustom 2",rank: ""},
 {date: "大会運営：",title: "NovaCup Vol.1",rank: ""},
 {date: "大会運営：",title: "NovaCup Vol.2",rank: ""},
+{date: "大会運営：",title: "WKC Vol.1",rank: ""},
 {date: "大会運営：",title: "STELLA CUP Vol.1",rank: ""}
 ]},
 
@@ -43,7 +44,7 @@ results: []
 
 yuiri: {
 role: "CREATOR",
-name: "MinatoYuiri",
+name: "Minato Yuiri",
 realName: "JOIND：2026.8",
 birthday: "BIRTHDAY：03/18",
 image: "player/yuiri.png",
@@ -53,8 +54,128 @@ twitch: "https://www.twitch.tv/yuiri00",
 description: "かわいいものに囲まれていたいかわいい大好きゲーマー",
 results: [
 {date: "大会出場：",title: "桜珂杯",rank: "2位"}
-]}
+]},
 
+kanzaki: {
+role: "CREATOR",
+name: "Kanzaki Ayaka",
+realName: "JOIND：2026.6",
+birthday: "BIRTHDAY：09/21",
+image: "player/kanzaki.png",
+x: "https://x.com/kknzkk1129",
+youtube: "https://www.youtube.com/@knzk_09",
+twitch: "https://www.twitch.tv/knzk_09",
+description: "ポジティブ思考？ゲーマー",
+results: [
+{date: "大会運営：",title: "STELLA CUP",rank: ""}
+]},
+
+nade: {
+role: "CREATOR",
+name: "Nade",
+realName: "JOIND：2026.8",
+birthday: "BIRTHDAY：07/31",
+image: "player/nade.png",
+x: "#",
+youtube: "#",
+twitch: "#",
+description: "Nidmegent Esportsイメージキャラクター",
+results: [
+]},
+
+denden: {
+role: "STREAMER",
+name: "Denden",
+realName: "JOIND：2025.6",
+birthday: "BIRTHDAY：05/25",
+image: "player/denden.png",
+x: "https://x.com/dedendeden0525",
+youtube: "https://www.youtube.com/@odendennnn",
+twitch: "https://www.twitch.tv/denden_fps",
+description: "",
+results: [
+{date: "大会出場：",title: "StreamerValorantCustom 1",rank: ""},
+{date: "大会実況：",title: "StreamerValorantCustom 2",rank: ""}
+]},
+
+hakuanoru: {
+role: "STREAMER",
+name: "HakuaNoru",
+realName: "JOIND：2025.8",
+birthday: "BIRTHDAY：04/20",
+image: "player/noru.png",
+x: "https://x.com/NORU_hakua",
+youtube: "#",
+twitch: "https://www.twitch.tv/hakua_noru",
+description: "ゲームと配信を楽しむ、個性派ストリーマー",
+results: [
+{date: "大会出場：",title: "StreamerValorantCustom 1",rank: ""},
+{date: "大会実況：",title: "StreamerValorantCustom 2",rank: ""}
+]},
+
+arufu: {
+role: "VALORANT",
+name: "arufu",
+realName: "JOIND：2025.8",
+birthday: "BIRTHDAY：09/29",
+image: "player/noru.png",
+x: "https://x.com/arfy_vl",
+youtube: "#",
+twitch: "https://www.twitch.tv/arfy_ry",
+description: "VALORANTシーンで存在感を放つ、実力派プレイヤー",
+results: [
+{date: "大会実況：",title: "StreamerValorantCustom 2",rank: ""}
+]},
+
+keto: {
+role: "VALORANT",
+name: "keto",
+realName: "JOIND：2025.3",
+birthday: "BIRTHDAY：07/20",
+image: "player/comingsoon.png",
+x: "https://x.com/keto7200840",
+youtube: "#",
+twitch: "https://www.twitch.tv/keto720",
+description: "",
+results: [
+]},
+
+hanannnnt: {
+role: "VALORANT",
+name: "Hanannnnt",
+realName: "JOIND：2025.4",
+birthday: "BIRTHDAY：09/25",
+image: "player/hanannnnt.png",
+x: "https://x.com/Hanannnnt",
+youtube: "#",
+twitch: "#",
+description: "明るい個性と親しみやすさで魅せるゲーマー",
+results: [
+{date: "大会出場：",title: "STAGE:0 VALORANT 2026",rank: "予選敗退"},
+{date: "大会出場：",title: "VALORANT UNIVERS HIGH CUP",rank: ""},
+{date: "大会出場：",title: "ESR GRAND PRIX VALORANT",rank: "C Tier Best.6"},
+{date: "大会出場：",title: "STAGE:0 VALORANT 2025",rank: "予選敗退"},
+{date: "大会運営：",title: "KCN 太鼓の達人大会",rank: ""},
+{date: "大会運営：",title: "KEST VALORANT 2025",rank: ""},
+{date: "大会運営：",title: "WKC Vol.1",rank: ""}
+]},
+
+nagi: {
+role: "VALORANT - LYXEN DIVISION",
+name: "Yamakaze Nagi",
+realName: "JOIND：2025.8",
+birthday: "BIRTHDAY：02/27",
+image: "player/nagi.png",
+x: "https://x.com/nagi_J0053",
+youtube: "https://www.youtube.com/@nagiyamakaze115",
+twitch: "https://www.twitch.tv/yamakazenagi115",
+description: "歌もゲームも駆け抜ける、アクティブVTuber",
+results: [
+{date: "大会主催：",title: "YAMAKAZENAGI BIRTHDAY CUSTOM 2026",rank: ""},
+{date: "大会出場：",title: "シルバー以下限定VALORANTカスタム",rank: ""},
+{date: "大会出場：",title: "StreamerValorantCustom 2",rank: ""},
+]},
+    
 }; // playerstats end
 
 /*==================================================
